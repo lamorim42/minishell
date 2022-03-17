c81fff41ea939198887c74130c0bc9e1b7f6b29a
luishads <luishads@gmail.com>
2022-03-17 09:29:05 -0300

    feat: readline header and structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 09:28:24 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/16 09:34:42 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/17 09:23:22 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <string.h>
+
+# define STDIN 0
+
+# define SEP "|"
+
+typedef struct s_node
+{
+	char	*char;
+	struct s_node *next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node *array;
+}	t_stack;
+
 
 #endif
