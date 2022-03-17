519232f4b6b60d46e59598956bcbeaaf54edf384
luishads <luishads@gmail.com>
2022-03-17 10:36:44 -0300

    refactor: coment function

--- src/list.c
+++ src/list.c
@@ -6,22 +6,20 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 09:46:03 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/17 09:46:51 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/17 10:13:10 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	new_node(char *str)
-{
-	int *ptr;
-
-	ptr = (char *)malloc(sizeof(char));
-
-	if (!ptr)
-		return (NULL);
-
-
-// miau 'kit cat' | miau
-// node -> "miau\0"
-}
+//void	new_node(char *str)
+//{
+//	int	*ptr;
+//	int	len;
+//
+//	ptr = (char *)malloc(sizeof(char));
+//	if (!ptr)
+//		return (NULL);
+//
+//
+//}
