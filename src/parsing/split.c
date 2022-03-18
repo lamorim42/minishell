82b7e48855078aadd7dcc97df127e3e0cb454fbc
luishads <luishads@gmail.com>
2022-03-18 19:40:01 -0300

    refactor: changes in split function
    fix somo bugs and generate list of arguments

--- src/parsing/split.c
+++ src/parsing/split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 09:28:34 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/18 10:23:42 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/18 19:37:48 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,28 @@ char	*split_line(char *line)
 	char	quot;
 
 	ptr_len = line;
-
 	cmd_line = malloc(sizeof(t_stack));
 	cmd_line->array = NULL;
+	content = handle_first_arg(&ptr_len);
+	cmd_line->array = new_node(content);
 	while (*ptr_len)
 	{
-		//printf("oii");
-		// miau
 		if (*ptr_len == 39 || *ptr_len == 34)
 		{
 			quot = *ptr_len;
 			content = handle_quotes(&ptr_len, quot);
-			printf("quots content: %s\n", content);
+			add_back_stack(content, &(cmd_line->array));
 		}
-		if (*ptr_len == 32 && (*(ptr_len + 1) != 39 && *(ptr_len + 1) != 34))
+		else if (*ptr_len == 32 && (*(ptr_len + 1) != 39 && *(ptr_len + 1) != 34))
 		{
 			ptr_len++;
 			content = handle_space(&ptr_len);
-			printf("space content: %s\n", content);
+			add_back_stack(content, &(cmd_line->array));
 		}
-		else if (*ptr_len == line[0])
-		{
-			content = handle_first_arg(&ptr_len);
-			printf("First_arg = %s\n", content);
-		}
-		add_back_stack(content, &(cmd_line->array));
-		ptr_len++;
+		else
+			ptr_len++;
 	}
+	printf("-------\n");
 	print_list(&cmd_line);
 	return (content);
 }
@@ -63,7 +58,7 @@ char	*handle_first_arg(char **ptr_len)
 		if ((*ptr_len)[i] == 32 || (*ptr_len)[i + 1] == '\0')
 		{
 			content = worddup(*ptr_len, i + 1);
-			*ptr_len += (i - 1);
+			*ptr_len += (i);
 			break ;
 		}
 		i++;
@@ -83,7 +78,7 @@ char	*handle_space(char **ptr_len)
 		if ((*ptr_len)[i] == 32 || (*ptr_len)[i + 1] == '\0')
 		{
 			content = worddup(*ptr_len, i + 1);
-			*ptr_len += (i - 1);
+			*ptr_len += (i);
 			break ;
 		}
 		i++;
