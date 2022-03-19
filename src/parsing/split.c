ed3082fd7d3a5a4a02c25c72ed1a827a9b1c3725
Daniela Monteiro <danimdf360@gmail.com>
2022-03-19 20:18:01 -0300

    refator: modification in stack for node

--- src/parsing/split.c
+++ src/parsing/split.c
@@ -3,47 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 09:28:34 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/18 19:37:48 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/19 19:59:16 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
-char	*split_line(char *line)
+t_node	*split_line(char *line)
 {
-	t_stack	*cmd_line;
+	t_node	*cmd_line;
 	char	*ptr_len;
 	char	*content;
 	char	quot;
 
 	ptr_len = line;
-	cmd_line = malloc(sizeof(t_stack));
-	cmd_line->array = NULL;
+	//cmd_line = malloc(sizeof(t_stack));
+	cmd_line = NULL;
 	content = handle_first_arg(&ptr_len);
-	cmd_line->array = new_node(content);
+	cmd_line = new_node(content);
 	while (*ptr_len)
 	{
 		if (*ptr_len == 39 || *ptr_len == 34)
 		{
 			quot = *ptr_len;
 			content = handle_quotes(&ptr_len, quot);
-			add_back_stack(content, &(cmd_line->array));
+			add_back_stack(content, &(cmd_line));
 		}
 		else if (*ptr_len == 32 && (*(ptr_len + 1) != 39 && *(ptr_len + 1) != 34))
 		{
 			ptr_len++;
 			content = handle_space(&ptr_len);
-			add_back_stack(content, &(cmd_line->array));
+			add_back_stack(content, &(cmd_line));
 		}
 		else
 			ptr_len++;
 	}
-	printf("-------\n");
-	print_list(&cmd_line);
-	return (content);
+	//printf("-------\n");
+	//print_list(&cmd_line);
+	return (cmd_line);
 }
 
 char	*handle_first_arg(char **ptr_len)
