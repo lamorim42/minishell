a6cf65ca75ae0eb775212fbd8700a83df38ac05f
luishads <luishads@gmail.com>
2022-03-21 16:53:12 -0300

    refactor: remove linked-list and stack
    remove stracts and functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/19 19:11:15 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/21 16:29:34 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,8 @@
 # include <string.h>
 # include "../Libft/libft.h"
 
-# define SEP "|><"
-
-
-typedef struct s_node
-{
- 	char	*str;
-	struct s_node *next;
-}	t_node;
-
-typedef struct s_stack
-{
-	t_node	**array_list;
-}	t_stack;
-
-void	parse(char *line, t_stack **stacks);
-t_node	*split_line(char *line);
-char	*worddup(const char *s, size_t len);
-int		has_double_quotation(char *str, char quot);
-char	*handle_quotes(char **ptr_len, char quot);
-void	return_error(void);
-char	*handle_space(char **ptr_len);
-
-//linked list
-t_node	*new_node(char *str);
-void	add_back_stack(char *content, t_node **head);
-void	print_list(t_node **stack);
-char	*handle_first_arg(char **ptr_len);
-void	clean_list(t_node **head);
-
-//parse linked_list
-void	cut_linked(t_stack **stacks, t_node *linked_list, int counter_red);
-t_node	*copy_list(t_node **linked_list);
+# define SPACE 32
+# define QUOT 34
+# define DQUOT 39
 
 #endif
