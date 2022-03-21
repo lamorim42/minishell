574369e8cc104195228ca40797fc634626cbb8ee
Daniela Monteiro <danimdf360@gmail.com>
2022-03-21 19:35:05 -0300

    Refactor: new functions in .h
    Add functions of tokenizer and return error

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/21 16:29:34 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/21 17:33:24 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,17 @@
 # include <string.h>
 # include "../Libft/libft.h"
 
-# define SPACE 32
+# define SPC 32
 # define QUOT 34
 # define DQUOT 39
 
+//tokenizer
+char	**token(char *line);
+size_t	count_words(char *s);
+char	*worddup(char **s);
+char	*quotdup(char **s);
+int	has_double_quotation(char *str, char quot);
+
+//error
+void	return_error(void);
 #endif
