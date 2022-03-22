a834692b4aaf721531e3ad7da3f05385eaa66638
Daniela Monteiro <danimdf360@gmail.com>
2022-03-22 15:23:07 -0300

    feat: Add table_red function
    Function that cut the pipe

--- src/main.c
+++ src/main.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 08:29:06 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/21 20:38:32 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/22 08:38:18 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 char	**table_cmds(char **tokens, int size);
+char	**table_red(char **tokens, int size);
 
 int	main(void)
 {
@@ -20,6 +21,7 @@ int	main(void)
 	char	*temp;
 	char	**tokens;
 	char	**table;
+	char	**table_redirect;
 	int		count_sep;
 	//int		i;
 
@@ -37,6 +39,7 @@ int	main(void)
 
 	tokens = token(line);
 	table = table_cmds(tokens, count_sep);
+	table_redirect = table_red(tokens, count_sep);
 
 	free(line);
 
@@ -51,32 +54,64 @@ char	**table_cmds(char **tokens, int size)
 
 	i = 0;
 	temp = tokens;
+	if (size == 0)
+	{
+		table = tokens;
+		return (table);
+	}
 	table = malloc(sizeof(char *) * (size + 2));
 	while (*temp)
 	{
-		printf("token: %s\n", *temp);
 		if (ft_strchr(*temp, '|'))
 		{
 			temp++;
 		}
 		else
 		{
-			printf("else do |\n");
 			table[i] = ft_strdup(*temp);
-			printf("table[%d] = %s\n", i, table[i]);
 			i++;
 			temp++;
 		}
 	}
 	table[i] = NULL;
-	temp = table;
+	/* temp = table;
 	while (*temp)
 	{
 		printf("temp: %s\n", *temp);
 		temp++;
-	}
-
+	} */
 	return (table);
+}
+
+char	**table_red(char **tokens, int size)
+{
+	char	**temp;
+	char	**array_red;
+	int		i;
+
+	temp = tokens;
+	i = 0;
+
+	array_red = malloc(sizeof(char *) * (size + 1));
+	while(*temp)
+	{
+		if (ft_strchr(*temp, '|') || ft_strchr(*temp, '>'))
+		{
+			printf("%d\n", i);
+			array_red[i] = ft_strdup(*temp);
+			i++;
+			temp++;
+		} else
+			temp++;
+	}
+	array_red[i] = NULL;
+	temp = array_red;
+	while (*temp)
+	{
+		printf("array_red: %s\n", *temp);
+		temp++;
+	}
+	return (array_red);
 }
 
 void	return_error(void)
