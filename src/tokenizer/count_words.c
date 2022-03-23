5a405ecba47f29b6b9dd8a476fec261e17519b7b
Daniela Monteiro <danimdf360@gmail.com>
2022-03-23 08:26:13 -0300

    refactor: modification in count_words
    new method to count

--- src/tokenizer/count_words.c
+++ src/tokenizer/count_words.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 19:43:15 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/22 19:59:46 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/23 08:14:09 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ size_t	count_words(char *s)
 	is_word = 0;
 	while (*s)
 	{
-		if (!is_word && !ft_strchr("| ", *s))
+		if (ft_strchr("|", *s))
+			words++;
+		else if (!is_word && !ft_strchr(" ", *s))
 		{
 			is_word = 1;
 			words++;
 		}
-		else if (is_word && ft_strchr("| ", *s))
+		else if (is_word && ft_strchr(" ", *s))
 			is_word = 0;
 		s++;
 	}
