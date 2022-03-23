f98f25ab15b0a9fa4b4909242e921e7226a0ee19
luishads <luishads@gmail.com>
2022-03-23 16:30:59 -0300

    feat: add array functions
    add array_len and free_array in proper file

---
+++ src/array.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   array.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 16:25:41 by lamorim           #+#    #+#             */
+/*   Updated: 2022/03/23 16:28:21 by lamorim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_array_len(char **array)
+{
+	int	len;
+	char	**temp;
+
+	temp = array;
+	len = 0;
+	while (*temp)
+	{
+		len++;
+		temp++;
+	}
+	return (len);
+}
+
+void	ft_free_array(char ***mtx)
+{
+	int	i;
+
+	i = 0;
+	while ((*mtx)[i])
+	{
+		free((*mtx)[i]);
+		i++;
+	}
+	free(*mtx);
+}
