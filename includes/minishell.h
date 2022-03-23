023ad4e3f75beda2404415f718a04ddd10522bee
luishads <luishads@gmail.com>
2022-03-23 16:30:31 -0300

    feat: add array functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/23 10:24:17 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/23 16:28:56 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,28 @@
 # define QUOT 34
 # define DQUOT 39
 
+typedef struct s_line {
+	char	*pipeline;
+	char	**tks;
+	char	**lex;
+}			t_line;
+
 //tokenizer
 char	**token(char *line);
 size_t	count_words(char *s);
 char	*worddup(char **s);
 char	*quotdup(char **s);
-int	has_double_quotation(char *str, char quot);
+int		has_double_quotation(char *str, char quot);
 
 //Lex
 char	**tokens_classification(char **tokens);
 
 //grammar
 void	rules_grammar(char	**lex_tokens);
+
+//array
+void	ft_free_array(char ***mtx);
+int		ft_array_len(char **array);
 
 //error
 void	return_error(void);
