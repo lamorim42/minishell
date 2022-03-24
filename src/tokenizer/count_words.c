0216c03f97141a0ec232b97721ce65e39a5a8fb2
Daniela Monteiro <danimdf360@gmail.com>
2022-03-24 17:28:09 -0300

    fix: word_dup count '|'

--- src/tokenizer/count_words.c
+++ src/tokenizer/count_words.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 19:43:15 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/24 17:13:29 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/24 17:26:59 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ size_t	count_words(char *s)
 	size_t	words;
 	int		i;
 
-	i = 1;
+	i = 0;
 	words = 0;
 	while (s[i])
 	{
-		if (s[i] == ' ' && s[i + 1] != ' ' && s[i + 1] != '\0')
+		if (ft_strchr(" |", s[i]) && s[i + 1] != ' ' && s[i + 1] != '\0')
 			words++;
 		i++;
 	}
