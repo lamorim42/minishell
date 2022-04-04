7a3afc5001c7dd20db25c9bd46e04eb4a3f798f8
Daniela Monteiro <danimdf360@gmail.com>
2022-04-04 14:51:12 -0300

    refactor: add protection in malloc of lex_tokens

--- src/parsing/lexical_analysis.c
+++ src/parsing/lexical_analysis.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexical_analysis.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 16:03:24 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/30 01:57:23 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/04/04 09:07:11 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	**lexical_analysis(char **tokens)
 
 	len = ft_array_len(tokens);
 	lex_tokens = malloc(sizeof(char *) * (len + 1));
+	if(lex_tokens == NULL)
+		return_error();
 	temp = lex_tokens;
 	while (*tokens)
 	{
