a2aad46f676264647a61770fffbcd8401a4a372d
Daniela Monteiro <danimdf360@gmail.com>
2022-04-12 18:07:02 -0300

    refactor: clean line

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,11 +6,12 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/11 20:11:14 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/04/12 17:26:25 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
 
 #endif
