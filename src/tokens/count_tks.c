b347caf3317dd9204b8c4db81ecbcd38facffada
Daniela Monteiro <danimdf360@gmail.com>
2022-04-12 18:07:51 -0300

    refactor: Modification in count_tks

--- src/tokens/count_tks.c
+++ src/tokens/count_tks.c
@@ -6,24 +6,34 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 20:37:57 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/11 20:47:00 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/04/12 18:00:05 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //# include "minishell.h"
+# include "../Libft/libft.h"
+int	count_tks(char *line);
+
 
 int	count_tks(char *line)
 {
-	int counter;
+	int	counter;
+	int	flag;
 
+	flag = 0;
 	counter = 0;
 	while (*line)
 	{
-		if (*line == ' ')
+		if (ft_strchr("|", *line))
 			counter++;
+		if (!flag && !ft_strchr("| ", *line))
+		{
+			flag = 1;
+			counter++;
+		}
+		else if (flag && ft_strchr("| ", *line))
+			flag = 0;
 		line++;
 	}
-	if (*line == '\0')
-		counter++;
 	return (counter);
 }
