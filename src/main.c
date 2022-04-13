0344ca7a7634c85c1200acf068ba51ff2b33029c
luishads <luishads@gmail.com>
2022-04-12 21:04:11 -0300

    tests: make test with main.c

--- src/main.c
+++ src/main.c
@@ -3,21 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 20:12:17 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/12 17:19:25 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/04/12 19:32:09 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "minishell.h"
-//int	count_words(char *line);
-
-/* int	main ()
-{
-
-
-} */
+//#include <stdio.h>
+//
+//int	count_tks(char *line);
+//
+//int	main ()
+//{
+//	char	*line;
+//	int		nbr;
+//
+//	line = "\"ls -a";
+//	nbr = count_tks(line);
+//	dprintf(2, "nbr = %d\n", nbr);
+//	return (0);
+//}
 
 
 
