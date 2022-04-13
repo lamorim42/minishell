565868463168baf7803ab9d3eddbfdc8f990b60a
luishads <luishads@gmail.com>
2022-04-12 21:05:05 -0300

    feat: finileze count_tks
    add functions and features to count tks

--- src/tokens/count_tks.c
+++ src/tokens/count_tks.c
@@ -3,22 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   count_tks.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 20:37:57 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/12 18:00:05 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/04/12 20:51:26 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //# include "minishell.h"
-# include "../Libft/libft.h"
-int	count_tks(char *line);
+#include "../Libft/libft.h"
 
+static void	update_counter_flag(int *counter, int *flag);
+static void	count_inside_quot_as_one_word(char **line);
 
 int	count_tks(char *line)
 {
-	int	counter;
-	int	flag;
+	int		counter;
+	int		flag;
 
 	flag = 0;
 	counter = 0;
@@ -26,14 +27,32 @@ int	count_tks(char *line)
 	{
 		if (ft_strchr("|", *line))
 			counter++;
-		if (!flag && !ft_strchr("| ", *line))
+		if (!flag && ft_strchr("\"\'", *line))
 		{
-			flag = 1;
-			counter++;
+			count_inside_quot_as_one_word(&line);
+			update_counter_flag(&counter, &flag);
 		}
+		else if (!flag && !ft_strchr("| ", *line))
+			update_counter_flag(&counter, &flag);
 		else if (flag && ft_strchr("| ", *line))
 			flag = 0;
 		line++;
 	}
 	return (counter);
+}
+
+static void	update_counter_flag(int *counter, int *flag)
+{
+	(*counter)++;
+	*flag = 1;
+}
+
+static void	count_inside_quot_as_one_word(char **line)
+{
+	char	quot;
+
+	quot = **line;
+	(*line)++;
+	while (*(*line + 1) && **line != quot)
+		(*line)++;
 }
