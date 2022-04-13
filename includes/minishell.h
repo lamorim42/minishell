bc8248176c8a48e18983b5de60d62d8726c3ae87
Daniela Monteiro <danimdf360@gmail.com>
2022-04-13 20:51:36 -0300

    feat: new functions heads

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,15 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/12 17:26:25 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/04/13 18:39:37 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../Libft/libft.h"
+
+int	count_tks(char *line);
 
 #endif
