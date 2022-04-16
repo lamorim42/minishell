a2ec3554dfb96316faa8a777be4a74932bc81dee
Daniela Monteiro <danimdf360@gmail.com>
2022-04-16 19:49:28 -0300

    feat: Add tokenizer header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,15 +6,17 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/13 18:39:37 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/04/16 18:56:40 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
 # include "../Libft/libft.h"
 
 int	count_tks(char *line);
+char	**tokenizer(char *line);
 
 #endif
