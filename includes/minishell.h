1f69b0df4b106d3f2c86abce17a524dc5b51eeea
Daniela Monteiro <danimdf360@gmail.com>
2022-04-21 18:12:07 -0300

    choce: new headers from .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/21 15:56:30 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/04/21 17:58:25 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,15 @@
 typedef struct s_line {
 	char	*str;
 	char	**tks;
+	char	**lex;
+	char	**cmds;
 	int		tks_nbr;
 }			t_line;
 
 int		count_tks(char *line);
 char	**tokenizer(t_line *line);
 char	**lexical_analyzer(t_line *line);
+int		sintax_analysis(char **lex);
+char	**clean_tokens(t_line *line);
 
 #endif
