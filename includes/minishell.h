ccc753830fa2cd2d71f34207517db08f280babdf
Daniela Monteiro <danimdf360@gmail.com>
2022-04-26 20:37:22 -0300

    echo: headers in .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:19:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/23 12:23:27 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/04/26 19:10:00 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../Libft/libft.h"
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
 
 typedef struct s_line {
 	char	*str;
@@ -26,13 +29,19 @@ typedef struct s_line {
 	char	*bin;
 	int		tks_nbr;
 	char	**envp;
-}			t_line;
+	int		pid;
+}				t_line;
 
 int		count_tks(char *line);
+void	signals(t_line *line);
 char	**tokenizer(t_line *line);
 char	**lexical_analyzer(t_line *line);
 int		sintax_analysis(char **lex);
 char	**clean_tokens(t_line *line);
+void	init_line(t_line *line);
+void	free_line(t_line *line);
+void	exec_path(t_line *line);
+void	init_fork(t_line *line);
 
 char	*path_finder(t_line *line);
 
