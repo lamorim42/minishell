cc2f6ac3cdf9473c306e0423440a2aff679f0e28
Daniela Monteiro <danimdf360@gmail.com>
2022-05-03 21:16:38 -0300

    refactor: Change in execve func

--- src/exec/exec_path.c
+++ src/exec/exec_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_path.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:04:40 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/02 19:35:28 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/05/03 21:06:20 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	exec_path(t_line *line)
 {
-	print_array("cmds", line->cmds);
-	if (!execve(line->bin, line->cmds, line->envp))
+	//print_array("cmds", line->ctks);
+	if (!execve(line->bin, line->cmds[1], line->envp))
 		free_line(line);
 }
 
