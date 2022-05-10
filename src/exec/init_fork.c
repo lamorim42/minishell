a26eba5b523332f441adb79a88302f042f4da893
Daniela Monteiro <danimdf360@gmail.com>
2022-05-10 20:30:44 -0300

    refactor: Creates line->fd in init_fork

--- src/exec/init_fork.c
+++ src/exec/init_fork.c
@@ -6,16 +6,21 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:25:53 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/04/25 20:34:23 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/05 20:34:50 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	init_fork(t_line *line)
+void	init_fork(t_line *line, int *i)
 {
+
+	if (pipe(line->fd) != 0)
+		dprintf(2, "pipe error\n");
 	line->pid = fork();
 	if (line->pid == 0)
-		exec_path(line);
+	{
+		exec_path(line, i);
+	}
 	waitpid(line->pid, NULL, 0);
 }
