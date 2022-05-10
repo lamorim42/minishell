6c833414170bff744b5f877549b5aaf72458751f
Daniela Monteiro <danimdf360@gmail.com>
2022-05-10 20:29:29 -0300

    refactor: Opening fork in exec_path

--- src/exec/exec_path.c
+++ src/exec/exec_path.c
@@ -6,16 +6,40 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:04:40 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/03 21:06:20 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/10 19:14:35 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_path(t_line *line)
+void	exec_path(t_line *line, int *i)
 {
-	//print_array("cmds", line->ctks);
-	if (!execve(line->bin, line->cmds[1], line->envp))
-		free_line(line);
-}
 
+	dprintf(2, "%d\noiiii", *i);
+	if ((*i) < line->tks_nbr)
+	{
+		if (*i == 0)
+		{
+			close(line->fd[0]);
+			dup2(line->fd[1], STDOUT_FILENO);
+			close(line->fd[1]);
+		}
+		else
+		{
+			dup2(line->fd[0], STDIN_FILENO);
+			close(line->fd[1]);
+			close(line->fd[0]);
+		}
+	}
+	//print_array("cmds", line->ctks);
+	if (*i == 0)
+	{
+		close(line->fd[0]);
+		if (!execve(line->bin, line->cmds[0], line->envp))
+			free_line(line);
+	} else
+	{
+		if (!execve(line->bin, line->cmds[2], line->envp))
+			free_line(line);
+	}
+}
