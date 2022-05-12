de5fc18613bcfeeebcf831208bfb2e10baa4e55e
Daniela Monteiro <danimdf360@gmail.com>
2022-05-11 21:09:47 -0300

    refactor: Add verification in opening pipe in init_fork

--- src/exec/init_fork.c
+++ src/exec/init_fork.c
@@ -6,21 +6,25 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:25:53 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/05 20:34:50 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:12:06 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	init_fork(t_line *line, int *i)
+void	init_fork(t_line *line, t_pipe_list *list)
 {
 
-	if (pipe(line->fd) != 0)
-		dprintf(2, "pipe error\n");
+	if (list->next && ft_strncmp(list->next->args[0], "PIPE", 4))
+	{
+		if (pipe(line->fd) != 0)
+			dprintf(2, "pipe error\n");
+	}
+
 	line->pid = fork();
 	if (line->pid == 0)
 	{
-		exec_path(line, i);
+		exec_path(line, list);
 	}
 	waitpid(line->pid, NULL, 0);
 }
