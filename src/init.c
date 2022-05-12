370d5f7d32f68d62af3115aad54c2dd0a2f7e7b0
luishads <luishads@gmail.com>
2022-05-12 19:41:24 -0300

    feat: add bzero to init line fuction

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:54:20 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/11 20:21:21 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/12 19:40:10 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void	init_line(t_line *line)
 	line->cmds = NULL;
 	line->str = NULL;
 	line->ctks = NULL;
+	ft_bzero(line->fd, 2);
 }
