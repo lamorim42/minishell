329df394ad9589c73b2a67000504b63a790ac601
Daniela Monteiro <danimdf360@gmail.com>
2022-05-11 21:09:07 -0300

    refactor: Add linked_list in exec_path

--- src/exec/exec_path.c
+++ src/exec/exec_path.c
@@ -6,40 +6,29 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:04:40 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/10 19:14:35 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:13:44 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_path(t_line *line, int *i)
+void	exec_path(t_line *line, t_pipe_list *list)
 {
 
-	dprintf(2, "%d\noiiii", *i);
-	if ((*i) < line->tks_nbr)
+	if (!ft_strncmp(list->next->args[0], "PIPE", 4))
 	{
-		if (*i == 0)
-		{
-			close(line->fd[0]);
-			dup2(line->fd[1], STDOUT_FILENO);
-			close(line->fd[1]);
-		}
-		else
-		{
-			dup2(line->fd[0], STDIN_FILENO);
-			close(line->fd[1]);
-			close(line->fd[0]);
-		}
-	}
-	//print_array("cmds", line->ctks);
-	if (*i == 0)
-	{
+		dup2(line->fd[0], STDIN_FILENO);
 		close(line->fd[0]);
-		if (!execve(line->bin, line->cmds[0], line->envp))
-			free_line(line);
-	} else
+		close(line->fd[1]);
+	} else if (!ft_strncmp(list->prev->args[0], "PIPE", 4))
 	{
-		if (!execve(line->bin, line->cmds[2], line->envp))
-			free_line(line);
+		printf("%s\n", "gatinho");
+		dup2(line->fd[1], STDOUT_FILENO);
+		close(line->fd[1]);
+		close(line->fd[0]);
 	}
+	//print_array("cmds", line->ctks)
+
+	if (!execve(list->bin, list->args, line->envp))
+		free_line(line);
 }
