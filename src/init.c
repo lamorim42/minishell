0a91443b0ae2387c4fc10ad019ae88f2ce109bb1
Daniela Monteiro <danimdf360@gmail.com>
2022-05-11 21:12:18 -0300

    refactor: delete bin of init.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:54:20 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/03 19:44:26 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:21:21 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	init_line(t_line *line)
 	line->tks_nbr = 0;
 	line->lex = NULL;
 	line->cmds = NULL;
-	line->bin = NULL;
 	line->str = NULL;
 	line->ctks = NULL;
 }
