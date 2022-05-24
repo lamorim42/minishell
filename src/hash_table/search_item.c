80f899f0fad624bac4fad450a4d11ad3aaae92b8
Daniela Monteiro <danimdf360@gmail.com>
2022-05-24 19:02:02 -0300

    feat: func for search item in hash

---
+++ src/hash_table/search_item.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_item.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 15:43:06 by dmonteir          #+#    #+#             */
+/*   Updated: 2022/05/23 22:05:06 by dmonteir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*search_item(t_hash_table **table, char *key)
+{
+	t_hash_item *current;
+	int			index;
+
+	index = hash_function(key);
+	current = (*table)->item[index];
+	while (current != NULL)
+	{
+		if (current->key && ft_strncmp(current->key, key, ft_strlen(key) + 1) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
