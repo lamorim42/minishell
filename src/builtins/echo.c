18cd3119392c7aee0f0150f29d8c0021664f5092
Daniela Monteiro <danimdf360@gmail.com>
2022-05-28 19:46:27 -0300

    feat: Builtin echo ok

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 07:11:02 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/05/28 19:08:37 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/05/28 19:45:58 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	echo_builtin(t_pipe_list *node)
 {
-	printf("%s\n", node->args[1]);
+	int i;
+	i = 1;
+	while (node)
+	{
+		if (node->args[i] == NULL)
+			ft_putchar_fd('\n', 2);
+		if (node->args[i])
+		{
+			ft_putstr_fd(node->args[i], 2);
+			ft_putchar_fd('\n', 2);
+		}
+		node = node->next;
+	}
 }
