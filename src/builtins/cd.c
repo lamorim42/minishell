a2d9628d329022ad4275b4d10e1b0f057886d86e
Daniela Monteiro <danimdf360@gmail.com>
2022-06-22 20:45:37 -0300

    refactor: Builtin cd ok

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,26 +6,51 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 19:17:50 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/06/22 15:41:19 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/06/22 20:42:23 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	update_var_env(t_hash_table **table, char *str_key, char *val);
 void	cd_builtin(t_pipe_list *node, t_hash_table **table)
 {
 	char	*str_pwd;
+	char	*str_oldpwd;
 
 	str_pwd = NULL;
-	//str_pwd = print_search(table, "PWD");
-
-	/* str_pwd = node->args[1];
-	table_delete(table, "PWD");
-	hash_insert(table, "PWD", str_pwd); */
-
-	if (chdir(node->args[1]) == -1)
+	str_oldpwd =  NULL;
+	str_pwd = node->args[1];
+	if (ft_strlen(node->args[0]) > 2)
+	{
+		printf("%s: command not found\n", node->args[0]);
+		//exit(127);
+	}
+	if (ft_strncmp(str_pwd, "~", 1) == 0)
+		str_pwd = search_item(table, "HOME");
+	if (chdir(str_pwd) == -1)
 	{
 		printf("bash: cd: %s: No such file or directory\n", node->args[1]);
-		exit(1);
 	}
+	else
+	{
+		str_oldpwd = search_item(table, "PWD");
+		if (ft_strncmp(str_pwd, "/home", 5) != 0)
+		{
+			str_oldpwd = ft_strjoin(str_oldpwd, "/");
+			str_pwd = ft_strjoin(str_oldpwd, str_pwd);
+			update_var_env(table, "PWD", str_pwd);
+		}
+		else
+		{
+			update_var_env(table, "OLDPWD", str_pwd);
+			update_var_env(table, "PWD", str_pwd);
+		}
+	}
+}
+
+static void	update_var_env(t_hash_table **table, char *str_key, char *val)
+{
+	table_delete(table, str_key);
+	hash_insert(table,str_key, val);
 }
