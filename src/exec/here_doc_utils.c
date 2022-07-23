7b779fc05b563ae1ef1a9e20ab5d0dc7da62895e
luishads <luishads@gmail.com>
2022-07-23 10:56:00 -0300

    refactor(style): putting here doc in norma

--- src/exec/here_doc_utils.c
+++ src/exec/here_doc_utils.c
@@ -6,7 +6,7 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 15:02:04 by lamorim           #+#    #+#             */
-/*   Updated: 2022/07/22 15:03:17 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/07/23 10:49:56 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,31 @@ void	here_doc_write(char *buffer, t_pipe_list *node)
 		write(node->fd[1], buffer, ft_strlen(buffer));
 	close(node->fd[0]);
 	close(node->fd[1]);
+}
+
+void	creat_here_doc(t_pipe_list *list)
+{
+	t_pipe_list	*node;
+
+	node = list;
+	while (node)
+	{
+		if (ft_strcmp_len(node->args[0], "HERE"))
+			if (pipe(node->fd) != 0)
+				perror("pipe()");
+		node = node->next;
+	}
+}
+
+void	close_here_doc(t_pipe_list *list)
+{
+	t_pipe_list	*node;
+
+	node = list;
+	while (node)
+	{
+		if (ft_strcmp_len(node->args[0], "HERE"))
+			close(node->fd[1]);
+		node = node->next;
+	}
 }
